4ce018d1387c287e9333033b72718529e57f4f81
el.na <el.na@dataportcrm.com>
2023-06-21 23:44:46 +0900

    Support next version of mod loader interfaces

--- src/mods-unpacked/shakerratto-LevelOfDifficulty/mod_main.gd
+++ src/mods-unpacked/shakerratto-LevelOfDifficulty/mod_main.gd
@@ -11,29 +11,29 @@ var ext_dir = ""
 var trans_dir = ""
 
 func _init(modLoader = ModLoader):
-	ModLoaderUtils.log_info("Init", MYMODNAME_LOG)
-	dir = modLoader.UNPACKED_DIR + MOD_DIR
+	ModLoaderLog.info("Init", MYMODNAME_LOG)
+	dir = ModLoaderMod.get_unpacked_dir() + MOD_DIR
 	ext_dir = dir + "extensions/"
 	trans_dir = dir + "translations/"
 	content_dir = dir + "content/"
 	content_data_dir = dir + "content_data/"
 	
 	# Add extensions
-	modLoader.install_script_extension(ext_dir + "ui/menus/run/difficulty_selection/difficulty_selection.gd")
-	modLoader.install_script_extension(ext_dir + "projectiles/bullet_enemy/enemy_projectile.gd")
-	modLoader.install_script_extension(ext_dir + "entities/structures/landmine/landmine.gd")
-	modLoader.install_script_extension(ext_dir + "entities/structures/turret/turret.gd")
-	modLoader.install_script_extension(ext_dir + "entities/units/player/player.gd")
-	#modLoader.install_script_extension(ext_dir + "entities/units/enemies/enemy.gd")
-	modLoader.install_script_extension(ext_dir + "entities/units/enemies/boss/boss.gd")
-	modLoader.install_script_extension(ext_dir + "zones/wave_manager.gd")
-	modLoader.install_script_extension(ext_dir + "singletons/run_data.gd")
-	modLoader.install_script_extension(ext_dir + "singletons/debug_service.gd")
-	modLoader.install_script_extension(ext_dir + "main.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/run/difficulty_selection/difficulty_selection.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "projectiles/bullet_enemy/enemy_projectile.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "entities/structures/landmine/landmine.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "entities/structures/turret/turret.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "entities/units/player/player.gd")
+	#ModLoaderMod.install_script_extension(ext_dir + "entities/units/enemies/enemy.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "entities/units/enemies/boss/boss.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "zones/wave_manager.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "singletons/run_data.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "singletons/debug_service.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "main.gd")
 	
 	# Add translations
-	modLoader.add_translation_from_resource(trans_dir + "/shakerratto_translations.en.translation")
-	modLoader.add_translation_from_resource(trans_dir + "/shakerratto_translations.ko.translation")
+	ModLoaderMod.add_translation(trans_dir + "shakerratto_translations.en.translation")
+	ModLoaderMod.add_translation(trans_dir + "shakerratto_translations.ko.translation")
 
 func _ready():
 	RunData.boss_1 = load(ext_dir + "entities/units/enemies/001/001.tscn")
