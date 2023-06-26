e17e5f5d60f4bea32ed4982912cab6d17ebe2d6a
rhajrs <rhajrs@gmail.com>
2023-06-26 22:46:15 +0900

    Fix effect bug for banner item

--- src/mods-unpacked/shakerratto-LevelOfDifficulty/extensions/singletons/debug_service.gd
+++ src/mods-unpacked/shakerratto-LevelOfDifficulty/extensions/singletons/debug_service.gd
@@ -4869,6 +4869,7 @@ func edit_items() -> void:
 		e.storage_method = 0
 		e.effect_sign = 3
 		item.effects.append(e)
+		
 		e = Effect.new()
 		e.key = "stat_attack_speed"
 		e.text_key = ""
@@ -4877,6 +4878,8 @@ func edit_items() -> void:
 		e.storage_method = 0
 		e.effect_sign = 3
 		item.effects.append(e)
+		
+		e = Effect.new()
 		e.key = "stat_lifesteal"
 		e.text_key = ""
 		e.value = -2
